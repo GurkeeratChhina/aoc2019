188b4e04e73cdcd518be467f51fbc82ae7c0b857
GurkeeratChhina <gurkeerat.chhina@gmail.com>
2025-01-20 20:49:13 -0800

    d7p2 done

--- solutions/d6.hs
+++ solutions/d6.hs
@@ -2,23 +2,93 @@ module Main where
 
 import System.TimeIt ( timeItT )
 import Text.Printf ( printf )
+import Data.Tree ( levels, Forest, Tree(Node) )
+import Data.List ( delete, find )
+import Data.List.Split ( splitOn )
 
 filename :: String
 filename = "data/d6.txt"
 
---adjust depending on structure of input file
-parse :: IO [Int]
-parse = map read . lines <$> readFile filename
+parse :: IO [(String, String)]
+parse = map ((\x -> (head x, last x)) . splitOn ")") . lines <$> readFile filename
 
 part1 :: IO Int
-part1 = sum <$> parse
+part1 = depths . makeTree "COM" <$> parse
 
 part2 :: IO Int
-part2 = sum <$> parse
+part2 = stepsBetween "YOU" "SAN" . makeTree "COM" <$> parse
 
+stepsBetween :: (Eq a) => a -> a -> Tree a -> Int
+stepsBetween start end t = height start t + height end t - 2* height (common start end t) t - 2
+
+height :: (Eq a) => a -> Tree a -> Int
+height label t = height' label (levels t)
+
+height' :: (Eq a) => a -> [[a]] -> Int
+height' _ [] = 0
+height' label (x:xs) = if label `elem` x then 0 else 1+height' label xs
+
+common :: (Eq a) => a -> a -> Tree a -> a
+common x y (Node root rest) = case find (\s -> hasNode x s && hasNode y s) rest of 
+    Just a -> common x y a
+    Nothing -> root
+
+depths :: Tree a -> Int
+depths = sum . zipWith (*) [0..] . map length . levels
+
+makeTree :: (Eq a) => a -> [(a,a)] -> Tree a
+makeTree root connections = head (addNodes [Node root []] connections )
+
+addNodes :: (Eq a) => Forest a -> [(a,a)] -> Forest a
+addNodes f [] = f
+addNodes f ((parent,child):rest)
+  | childIsRoot f child && hasParent f parent = addNodes (joinTrees f parent child) rest
+  | not (childIsRoot f child) && hasParent f parent = addNodes (addLeaf f parent child) rest
+  | childIsRoot f child && not (hasParent f parent) = addNodes (addAbove f parent child) rest
+  | not (childIsRoot f child) && not (hasParent f parent) = addNodes (addNew f parent child) rest
+  | otherwise = []
+
+childIsRoot :: (Eq a) => Forest a -> a -> Bool
+childIsRoot [] _ = False
+childIsRoot (t:ts) child = root == child || childIsRoot ts child where (Node root _) = t
+
+isRoot :: (Eq a) => a -> Tree a -> Bool
+isRoot r (Node root _) = r == root
+
+hasParent :: (Eq a) => Forest a -> a -> Bool
+hasParent [] _ = False
+hasParent (t:ts) parent = hasNode parent t || hasParent ts parent
+
+hasNode :: (Eq a) => a -> Tree a -> Bool
+hasNode label (Node root rest)  = root == label || any (hasNode label) rest
+
+addNew :: Forest a -> a -> a -> Forest a
+addNew f p c = Node p [Node c []] : f
+
+addAbove :: (Eq a) => Forest a -> a -> a -> Forest a
+addAbove [] _ _ = []
+addAbove (t:ts) p c = if root == c then Node p [t]:ts else t:addAbove ts p c where (Node root _) = t
+
+addLeaf :: (Eq a) => Forest a -> a -> a -> Forest a
+addLeaf [] _ _ = []
+addLeaf (t:ts) p c = if hasNode p t then addLeaf' p c t:ts else t:addLeaf ts p c
+
+addLeaf' :: (Eq a) => a -> a -> Tree a -> Tree a
+addLeaf' p c (Node root rest) = if root == p then Node root (Node c []:rest) else Node root (map (addLeaf' p c) rest)
+
+joinTrees :: (Eq a) => Forest a -> a -> a -> Forest a
+joinTrees f p c = findAndCombine (isRoot c) (hasNode p) (joinTrees' p) f
+
+joinTrees' :: (Eq a) => a -> Tree a -> Tree a -> Tree a
+joinTrees' label toAdd (Node root rest) = if root == label then Node root (toAdd:rest) else Node root (map (joinTrees' label toAdd) rest)
+
+findAndCombine :: (Eq p) => (p -> Bool) -> (p -> Bool)  -> (p->p->p) -> [p] -> [p]
+findAndCombine c1 c2 f xs = case (find c1 xs, find c2 xs) of
+    (Just a, Just b) -> f a b:(delete a . delete b $ xs)
+    (_,_) -> xs
 
 main :: IO ()
-main = do 
+main = do
     (time1, result1) <- timeItT part1
     printf "The answer to Day 6 part 2 is: %-10d, calculated in: %.6f ms\n" result1 (time1*1000)
     (time2, result2) <- timeItT part2
--- solutions/d7.hs
+++ solutions/d7.hs
@@ -2,24 +2,100 @@ module Main where
 
 import System.TimeIt ( timeItT )
 import Text.Printf ( printf )
+import Data.List.Split ( splitOn )
+import Data.List ( permutations )
 
 filename :: String
 filename = "data/d7.txt"
 
---adjust depending on structure of input file
 parse :: IO [Int]
-parse = map read . lines <$> readFile filename
+parse = map read . splitOn "," <$> readFile filename
+
+instr :: [Int] -> Int-> [Int] -> [Int]
+instr input index l
+  | code == 99 = []
+  | code == 1 = instr input (index+4) (replace (l!!(index+3)) (paramMode mode1 (index+1) l + paramMode mode2 (index+2) l) l )
+  | code == 2 = instr input (index+4) (replace (l!!(index+3)) (paramMode mode1 (index+1) l * paramMode mode2 (index+2) l) l )
+  | code == 3 = instr (tail input) (index+2) (replace (l!!(index+1)) (head input) l)
+  | code == 4 = paramMode mode1 (index+1) l:instr input (index+2) l
+  | code == 5 = if paramMode mode1 (index+1) l /= 0 then instr input (paramMode mode2 (index+2) l) l else instr input (index+3) l
+  | code == 6 = if paramMode mode1 (index+1) l == 0 then instr input (paramMode mode2 (index+2) l) l else instr input (index+3) l
+  | code == 7 = if paramMode mode1 (index+1) l < paramMode mode2 (index+2) l
+    then instr input (index+4) (replace (l!!(index+3)) 1 l )
+    else instr input (index+4) (replace (l!!(index+3)) 0 l )
+  | code == 8 = if paramMode mode1 (index+1) l == paramMode mode2 (index+2) l
+    then instr input (index+4) (replace (l!!(index+3)) 1 l )
+    else instr input (index+4) (replace (l!!(index+3)) 0 l )
+  | otherwise = [0]
+  where (code, mode1, mode2) = ((l!!index) `mod` 100, ((l!!index) `div` 100) `mod` 10, ((l!!index) `div` 1000) `mod` 10)
+
+runInstr :: State -> State
+runInstr (State h index l ins outs)
+  | h = State False 0 [] [] []
+  | code == 99 = State True index l ins outs
+  | code == 1 = runInstr (State h (index+4) (replace (l!!(index+3)) (paramMode mode1 (index+1) l + paramMode mode2 (index+2) l) l ) ins outs)
+  | code == 2 = runInstr (State h (index+4) (replace (l!!(index+3)) (paramMode mode1 (index+1) l * paramMode mode2 (index+2) l) l ) ins outs)
+  | code == 3 && null ins = State h index l ins outs
+  | code == 3 && not (null ins) = runInstr (State h (index+2) (replace (l!!(index+1)) (head ins) l) (tail ins) outs)
+  | code == 4 = runInstr (State h (index+2) l ins (paramMode mode1 (index+1) l:outs))
+  | code == 5 = if paramMode mode1 (index+1) l /= 0 then runInstr (State h (paramMode mode2 (index+2) l) l ins outs) else runInstr (State h (index+3) l ins outs)
+  | code == 6 = if paramMode mode1 (index+1) l == 0 then runInstr (State h (paramMode mode2 (index+2) l) l ins outs) else runInstr (State h (index+3) l ins outs)
+  | code == 7 = if paramMode mode1 (index+1) l < paramMode mode2 (index+2) l
+    then runInstr (State h (index+4) (replace (l!!(index+3)) 1 l ) ins outs)
+    else runInstr (State h (index+4) (replace (l!!(index+3)) 0 l ) ins outs)
+  | code == 8 = if paramMode mode1 (index+1) l == paramMode mode2 (index+2) l
+    then runInstr (State h (index+4) (replace (l!!(index+3)) 1 l ) ins outs)
+    else runInstr (State h (index+4) (replace (l!!(index+3)) 0 l ) ins outs)
+  | otherwise = State False 0 [] [] []
+  where (code, mode1, mode2) = ((l!!index) `mod` 100, ((l!!index) `div` 100) `mod` 10, ((l!!index) `div` 1000) `mod` 10)
+
+replace :: Int -> a -> [a] -> [a]
+replace index value l = x ++ value:tail y where (x, y) = splitAt index l
 
 part1 :: IO Int
-part1 = sum <$> parse
+part1 = part1' <$> parse
+
+part1' :: [Int] -> Int
+part1' l = maximum $ map (repeatInstr 5 0 l) (permutations [0..4])
+
+repeatInstr :: Int -> Int -> [Int] -> [Int] -> Int
+repeatInstr _ input _ [] = input
+repeatInstr n input instrs (x:xs)  = repeatInstr (n-1) (last . instr [x, input] 0 $ instrs) instrs xs
 
 part2 :: IO Int
-part2 = sum <$> parse
+part2 = part2' <$> parse
 
+part2' :: [Int] -> Int
+part2' l = maximum $ map (loopingInstr 0 . buildStates True l) (permutations [5..9])
+
+buildStates :: Bool -> [Int] -> [Int] -> [State]
+buildStates _ _ [] = []
+buildStates True ls (x:xs) = State False 0 ls [x, 0] []:buildStates False ls xs
+buildStates False ls (x:xs) = State False 0 ls [x] []:buildStates False ls xs
+
+paramMode :: Int -> Int -> [Int] -> Int
+paramMode 0 index l = l!!(l!!index)
+paramMode 1 index l = l!!index
+paramMode x _ _ = x
+
+data State = State {halted :: Bool, i :: Int, c :: [Int], inputs :: [Int], outputs :: [Int]} deriving (Show)
+
+loopingInstr :: Int-> [State] -> Int
+loopingInstr index states = if all halted states then last $ inputs (states !! index) else let (curr, next) = computeStates (states!!index) (states!!nextIndex) in loopingInstr nextIndex (replace index curr . replace nextIndex next $ states)
+  where nextIndex = (index+1) `mod` length states
+
+computeStates :: State -> State -> (State, State)
+computeStates first (State h1 i1 code1 inputs1 outputs1) = (State h2 i2 code2 inputs2 [] , State h1 i1 code1 (inputs1++outputs2) outputs1)
+  where (State h2 i2 code2 inputs2 outputs2) = runInstr first
 
 main :: IO ()
-main = do 
+main = do
+    -- parsed <- parse 
+    -- let temp = buildStates True parsed [9]
+    -- print temp
+    -- let processed = runInstr (temp!!0)
+    -- print processed
     (time1, result1) <- timeItT part1
-    printf "The answer to Day 7 part 2 is: %-10d, calculated in: %.6f ms\n" result1 (time1*1000)
+    printf "The answer to Day 7 part 1 is: %-10d, calculated in: %.6f ms\n" result1 (time1*1000)
     (time2, result2) <- timeItT part2
     printf "The answer to Day 7 part 2 is: %-10d, calculated in: %.6f ms\n" result2 (time2*1000)
